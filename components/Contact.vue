9ed4580e40d3568efdd0c1a0bfe5e81b232292ed
TranMinhTien271202 <tientienno2@gmail.com>
2023-12-06 10:08:38 +0700

    contact-us

--- components/Contact.vue
+++ components/Contact.vue
@@ -2,14 +2,11 @@
 const config = useRuntimeConfig();
 
 const formState = reactive({
-  firstname: "",
-  lastname: "",
+  yourname: "",
   phonenumber: "",
   email: "",
-  title: "",
+  subject: "",
   message: "",
-  demands: [],
-  souece: 12
 });
 
 const validatorPhonenumber = async (_rule, value) => {
@@ -27,16 +24,15 @@ const validatorEmail = async (_rule, value) => {
 };
 
 const rules = {
-  firstname: { required: true, message: "Tên không được để trống" },
-  lastname: { required: true, message: "Họ không được để trống" },
-  phonenumber: [{ required: true, message: "Số điện thoại không được để trống" }, { validator: validatorPhonenumber }],
+  yourname: { required: true, message: "Your name required" },
+  subject: { required: true, message: "Subject required" },
+  phonenumber: [{ required: true, message: "Phone required" }, { validator: validatorPhonenumber }],
   email: [
-    { required: true, message: "Email không được để trống" },
-    { type: "email", message: "Email không đúng định dạng" },
+    { required: true, message: "Email required" },
+    { type: "email", message: "Email format is wrong" },
     { validator: validatorEmail },
   ],
-  title: { required: true, message: "Chức vụ không được để trống" },
-  message: { required: true, message: "Lời nhắn không được để trống" },
+  message: { required: true, message: "Message required" },
 };
 
 let errors = {};
@@ -102,91 +98,74 @@ onMounted(() => {
   <div class="xl:flex gap-8">
     <div class="basis-1/2">
       <div class="p-10">
-        <h3 class="border-b-2 pb-4">Quy trình triển khai</h3>
+        <h3 class="font-[700]">Contact us</h3>
         <div class="pt-5 flex">
-          <h3 class="text-primary">01</h3>
-          <div class="pt-2 pl-5">Tạo tài khoản</div>
+          <div class="font-[400]">We can answer any of your questions, provide product demos and find the perfect
+            package for you.</div>
         </div>
-        <div class="pt-5 flex">
-          <h3 class="text-primary">02</h3>
-          <div class="pt-2 pl-5">Dùng thử sản phẩm</div>
+        <div class="pt-5 flex items-center">
+          <h3 class="text-primary"><img src="/images/icons/location.svg" alt=""></h3>
+          <div class="font-[400] pl-2">Hanoi office: No 41, Phu Kieu 1, Kieu Mai, Bac Tu Liem, Ha Noi</div>
         </div>
-        <div class="pt-5 flex">
-          <h3 class="text-primary">03</h3>
-          <div class="pt-2 pl-5">Đề xuất gói sản phẩm phù hợp</div>
+        <div class="pt-5 flex items-center">
+          <h3 class="text-primary"><img src="/images/icons/location.svg" alt=""></h3>
+          <div class="font-[400] pl-2">HCM offcice: No 10, Pham Van Hai, Tan Binh, HCM</div>
         </div>
-        <div class="pt-5 flex">
-          <h3 class="text-primary">04</h3>
-          <div class="pt-2 pl-5">Lên phương án đề xuất</div>
+        <div class="pt-5 flex items-center">
+          <h3 class="text-primary"><img src="/images/icons/email.svg" alt=""></h3>
+          <div class="font-[400] pl-2"><a href="mailto:contact@datanomics.com.vn">Email: contact@datanomics.com.vn</a>
+          </div>
         </div>
-        <div class="pt-5 flex">
-          <h3 class="text-primary">05</h3>
-          <div class="pt-2 pl-5">Gửi bản báo giá</div>
+        <div class="pt-5 flex items-center">
+          <h3 class="text-primary"><img src="/images/icons/tel.svg" alt=""></h3>
+          <div class="font-[400] pl-2"><a href="tel:(+84)2432007319">Tel: (+84)2432007319</a></div>
         </div>
-        <div class="pt-5 flex">
-          <h3 class="text-primary">06</h3>
-          <div class="pt-2 pl-5">Ký hợp đồng & triển khai</div>
+        <div class="pt-5 flex items-center">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.7338866099644!2d105.74872507471474!3d21.04333128727904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313454ec6751e303%3A0xfe0d54a385a89372!2zNDEgUC4gUGjDuiBLaeG7gXUsIFBow7pjIERp4buFbiwgQuG6r2MgVOG7qyBMacOqbSwgSMOgIE7hu5lpLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1701829928015!5m2!1svi!2s"
+            width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </div>
     </div>
     <div class="basis-1/2">
       <section id="contact-us" class="bg-white p-10 rounded-[48px] shadow-lg shadow-[#c6c6c6]">
         <div class="max-xl:text-center">
-          <h3 class="font-semibold">Hãy kết nối với chúng tôi ngay</h3>
-          <p class="subtitle-2 text-[#333333] mt-4">
-            Vui lòng điền thông tin của bạn ở dưới đây để đăng ký dùng thử hoặc nhận tư vấn miễn phí.
-          </p>
+          <h3 class="font-semibold">Send Your Message To Us</h3>
 
-          <a-form
-            ref="formRef"
-            class="w-full mt-5 max-xl:text-left"
-            layout="vertical"
-            :model="formState"
-            :rules="rules"
-            @finish="onSubmit"
-          >
-            <div class="sm:flex">
-              <a-form-item label="Tên" class="sm:w-1/2" name="firstname">
-                <a-input v-model:value="formState.firstname" placeholder="Nhập tên" />
-              </a-form-item>
-
-              <a-form-item label="Họ" class="sm:w-1/2 sm:ml-6" name="lastname">
-                <a-input v-model:value="formState.lastname" placeholder="Nhập họ" />
-              </a-form-item>
-            </div>
-
-            <a-form-item label="Số điện thoại" class="flex-1" name="phonenumber">
-              <a-input v-model:value="formState.phonenumber" placeholder="Nhập số điện thoại" />
+          <a-form ref="formRef" class="w-full mt-5 max-xl:text-left" layout="vertical" :model="formState" :rules="rules"
+            @finish="onSubmit">
+            <a-form-item label="Your Name" name="yourname">
+              <a-input v-model:value="formState.yourname" placeholder="Your Name" />
             </a-form-item>
 
-            <a-form-item label="Email" class="flex-1" name="email">
-              <a-input v-model:value="formState.email" placeholder="Nhập email" />
+            <a-form-item label="Your Email" class="flex-1" name="email">
+              <a-input v-model:value="formState.email" placeholder="Your Email" />
             </a-form-item>
 
-            <a-form-item label="Sản phẩm quan tâm" class="block flex-1" name="demands">
-              <a-checkbox-group
-                v-model:value="formState.demands"
-                class="w-full grid grid-cols-[repeat(auto-fit,minmax(230px,1fr))] gap-x-1 gap-y-2"
-              >
-                <a-checkbox class="!ml-0" v-for="item in demands" :value="+item.id">{{ item.description }}</a-checkbox>
-              </a-checkbox-group>
+            <a-form-item label="Your Number" class="flex-1" name="phonenumber">
+              <a-input v-model:value="formState.phonenumber" placeholder="Your Number" />
             </a-form-item>
 
-            <a-form-item label="Lời nhắn" name="message">
-              <a-textarea v-model:value="formState.message" placeholder="Để lại lời nhắn" :rows="5"></a-textarea>
+            <a-form-item label="Subject" class="flex-1" name="subject">
+              <a-input v-model:value="formState.subject" placeholder="Subject" />
+            </a-form-item>
+
+            <a-form-item label="Your Message" name="message">
+              <a-textarea v-model:value="formState.message" placeholder="Your Message" :rows="5"></a-textarea>
             </a-form-item>
 
             <a-form-item class="text-right">
-              <button class="primary w-full" type="submit" :disabled="loading">Gửi thông tin</button>
+              <button class="primary w-full" type="submit" :disabled="loading">Send Message</button>
             </a-form-item>
           </a-form>
 
           <a-modal :visible="visibleResultModal" :footer="null" :closable="false">
-            <h3 class="text-primary text-center">Cám ơn bạn đã quan tâm giải pháp của Miraway.</h3>
-            <div class="mt-2 text-center">Chúng tôi sẽ liên hệ bạn thong thời gian sớm nhất!</div>
+            <h3 class="text-primary text-center">Thank you for your interest in Datamonics.</h3>
+            <div class="mt-2 text-center">We will contact you as soon as possible!</div>
 
             <div class="text-center mt-6">
-              <button class="primary" @click="visibleResultModal = false">Đóng</button>
+              <button class="primary" @click="visibleResultModal = false">Close</button>
             </div>
           </a-modal>
         </div>
---
+++ pages/contact-us.vue
@@ -0,0 +1,60 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="home-page">
+    <section class="rounded-[48px] relative">
+      <div class="max-sm:hidden">
+        <nuxt-picture
+          loading="lazy"
+          src="/images/contact-us-banner.png"
+          :imgAttrs="{ class: 'w-full', alt: 'home-banner' }"
+          class="w-full"
+        />
+      </div>
+      <div class="sm:hidden -mx-20">
+        <nuxt-picture
+          loading="lazy"
+          src="/images/contact-us-banner.png"
+          :imgAttrs="{ class: 'w-full', alt: 'home-banner-mobile' }"
+          class="w-full"
+        />
+      </div>
+
+      <div
+        class="absolute max-2xl:bottom-[15px] top-1/2 max-md:top-[90px] max-md:left-[16px] left-[80px] md:-translate-y-1/2"
+      >
+        <h1 class="text-secondary max-sm:text-white">Data Driven Origanization</h1>
+
+        <p
+          class="w-2/4 text-[16px] text-secondary max-sm:text-[12px] max-sm:w-full font-[600] max-sm:text-white mt-2 max-sm:mt-4 text-black"
+        >
+          We aspire to be the partner of choice in your transformation into a data-driven organization and enable you to
+          manage and monetize data through effective use of people, process and technology.
+        </p>
+
+        <div class="relative pt-3 flex gap-6">
+          <nuxt-link to="#">
+            <button
+              class="rounded-[12px] w-[209px] h-[56px] bg-primary text-secondary border-e-0 max-md:hidden border-none"
+              aria-label="solution-miraway"
+            >
+              GET STARTED
+            </button>
+          </nuxt-link>
+          <nuxt-link to="#">
+            <button
+              class="rounded-[12px] w-[209px] h-[56px] text-secondary border-2 border-white max-md:hidden"
+              aria-label="solution-miraway"
+            >
+              LEARN MORE
+            </button>
+          </nuxt-link>
+        </div>
+      </div>
+    </section>
+
+    <Contact class="mt-[80px] 2xl:container mx-auto" />
+  </div>
+</template>
