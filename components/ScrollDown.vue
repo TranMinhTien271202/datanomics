c9e89f5b6b8105b7999eb9f440b0271d4013f56d
TranMinhTien271202 <tientienno2@gmail.com>
2023-12-06 14:13:50 +0700

    fix scroll down

---
+++ components/ScrollDown.vue
@@ -0,0 +1,14 @@
+<script setup>
+const emit = defineEmits(["scrollToTop"]);
+const scrollToTop = () => {
+    emit('scrollToTop')
+}
+</script>
+<template>
+    <NuxtLink @click="scrollToTop"
+        class="absolute left-1/2 bottom-[60px] -translate-x-1/2 flex flex-col gap-[8px] items-center max-sm:hidden">
+        <p class="text-white font-bold">Scroll Down</p>
+        <nuxt-picture loading="lazy" src="/images/our-service/Arrow-down.png" :imgAttrs="{ alt: 'scroll-down' }"
+            width="16" />
+    </NuxtLink>
+</template>
